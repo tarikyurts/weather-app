d0e4ba176606d5d11fd8c207ec858530e76ae73c
tarikyurts <56727073+tarikyurts@users.noreply.github.com>
2021-02-01 09:11:25 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+async function weather(url) {
+  const resp = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=43906a8d2c914826a17103725213101&q=${url}`,
+    {
+      headers: {
+        Authorization: "Bearer 43906a8d2c914826a17103725213101",
+      },
+    }
+  );
+  const result = await resp.json();
+  return result;
+}
+
+const istanbul = document.getElementById("istanbul");
+const edirne = document.getElementById("edirne");
+const ankara = document.getElementById("ankara");
+const tekirdag = document.getElementById("tekirdag");
+
+async function app() {
+  async function cityAPI(city) {
+    return await weather(city);
+  }
+
+  function citybox(city, cityAPI, name, temp, image) {
+    var img = document.createElement("img");
+    var name = document.createElement("span");
+    name.classList.add('city-name');
+    var temp = document.createElement("span");
+    temp.classList.add('city-temp');
+    img.src = image;
+    city.appendChild(name);
+    city.appendChild(img);
+    city.appendChild(temp);
+    name.innerHTML = cityAPI.location.name;
+    temp.innerHTML = Math.floor(cityAPI.current.temp_c) + '°C';
+  }
+  const istanbulAPI = await cityAPI("istanbul");
+  const edirneAPI = await cityAPI("edirne");
+  const ankaraAPI = await cityAPI("ankara");
+  const tekirdagAPI = await cityAPI("tekirdag");
+
+  console.log(istanbulAPI);
+  console.log(istanbulAPI.location.name);
+  console.log(istanbulAPI.current.temp_c);
+
+  citybox(
+    istanbul,
+    istanbulAPI,
+    istanbulAPI.location.name,
+    istanbulAPI.current.temp_c,
+    istanbulAPI.current.condition.icon
+  );
+
+  citybox(
+    edirne,
+    edirneAPI,
+    edirneAPI.location.name,
+    edirneAPI.current.temp_c,
+    edirneAPI.current.condition.icon
+  );
+
+  citybox(
+    ankara,
+    ankaraAPI,
+    ankaraAPI.location.name,
+    ankaraAPI.current.temp_c,
+    ankaraAPI.current.condition.icon
+  );
+
+  citybox(
+    tekirdag,
+    tekirdagAPI,
+    tekirdagAPI.location.name,
+    tekirdagAPI.current.temp_c,
+    tekirdagAPI.current.condition.icon
+  );
+}
+
+app();
